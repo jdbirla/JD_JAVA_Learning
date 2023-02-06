c14dc8970a3406cad9fafa02c723792b0376142f
JDBIRLA <69948118+jdbirla@users.noreply.github.com>
2023-02-06 10:54:27 +0530

    commit

--- Java_8/Java_8_In_Action/Java8InAction-master/src/main/java/lambdaasinaction/jd/JavaCodingInterview.java
+++ Java_8/Java_8_In_Action/Java8InAction-master/src/main/java/lambdaasinaction/jd/JavaCodingInterview.java
@@ -30,6 +30,39 @@ public class JavaCodingInterview {
 	}
 
 	public static void main(String[] args) {
+		
+		System.out.println("-------------------------------JDTraversing----------------------------------------------");
+
+		System.out.println("Que1:How to print keys & values of a Map in Java 8 ?");
+
+		strMap.keySet().stream().iterator().forEachRemaining(System.out::println);
+		System.out.println("-----------------------------------------------------");
+
+		strMap.values().stream().forEach(a -> System.out.println(a));
+
+		System.out.println("Que2: How to iterate a Map containing a List of String in Java 8 ?");
+		Map<String, List<String>> listOfAnimals = new HashMap<String, List<String>>();
+
+		// List #1: Creating list of Animals in Mammals Category
+		List<String> listOfMammals = Arrays.asList("Cat", "Dog", "Monkey", "Cow");
+		// Adding listOfMammals into Mammal's Category
+		listOfAnimals.put("Mammals", listOfMammals);
+
+		// List #2: Creating list of Animals in Birds Category
+		List<String> listOfBirds = Arrays.asList("Crow", "Parrot", "Peacock", "Flamingo");
+		// Adding listOfMammals into Bird's Category
+		listOfAnimals.put("Birds", listOfBirds);
+
+		// List #3: Creating list of Animals in Reptiles Category
+		List<String> listOfReptiles = Arrays.asList("Lizard", "Turtle", "Crocodile", "Python");
+		// Adding listOfMammals into Reptile's Category
+		listOfAnimals.put("Reptiles", listOfReptiles);
+
+		listOfAnimals.forEach((k, v) -> System.out.println(k + v));
+
+		System.out.println(
+				"-=============================================================================================================");
+		
 
 		System.out.println(
 				"----------------------------------------JDCounting----------------------------------------------");
@@ -152,6 +185,16 @@ public class JavaCodingInterview {
 		HashSet<Integer> s = new HashSet<Integer>();
 
 		myList2.stream().filter(a -> !s.add(a)).forEach(System.out::println);
+		
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 2, 3, 1);
+
+		Map<Integer, Long> counted = list.stream()
+			.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		List<Integer> result = counted.entrySet().stream()
+			.filter(entry -> entry.getValue() == 1)
+			.map(Map.Entry::getKey)
+			.collect(Collectors.toList());
 
 		System.out.println("----------------------------------------------------");
 		myList2.stream().filter(a -> !s.add(a)).collect(Collectors.toSet()).forEach(System.out::println);
@@ -286,37 +329,7 @@ public class JavaCodingInterview {
 		System.out.println(
 				"-=============================================================================================================");
 
-		System.out.println("-------------------------------JDTraversing----------------------------------------------");
-
-		System.out.println("Que1:How to print keys & values of a Map in Java 8 ?");
-
-		strMap.keySet().stream().iterator().forEachRemaining(System.out::println);
-		System.out.println("-----------------------------------------------------");
-
-		strMap.values().stream().forEach(a -> System.out.println(a));
-
-		System.out.println("Que2: How to iterate a Map containing a List of String in Java 8 ?");
-		Map<String, List<String>> listOfAnimals = new HashMap<String, List<String>>();
-
-		// List #1: Creating list of Animals in Mammals Category
-		List<String> listOfMammals = Arrays.asList("Cat", "Dog", "Monkey", "Cow");
-		// Adding listOfMammals into Mammal's Category
-		listOfAnimals.put("Mammals", listOfMammals);
-
-		// List #2: Creating list of Animals in Birds Category
-		List<String> listOfBirds = Arrays.asList("Crow", "Parrot", "Peacock", "Flamingo");
-		// Adding listOfMammals into Bird's Category
-		listOfAnimals.put("Birds", listOfBirds);
-
-		// List #3: Creating list of Animals in Reptiles Category
-		List<String> listOfReptiles = Arrays.asList("Lizard", "Turtle", "Crocodile", "Python");
-		// Adding listOfMammals into Reptile's Category
-		listOfAnimals.put("Reptiles", listOfReptiles);
-
-		listOfAnimals.forEach((k, v) -> System.out.println(k + v));
-
-		System.out.println(
-				"-=============================================================================================================");
+		
 
 		System.out.println("-------------------------------JDReducing----------------------------------------------");
 
